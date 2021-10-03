e00f075341f0acf99bd13a3aace2b734021f3c9f
Dhwaj Gupta <dhwajgupta27@gmail.com>
2021-10-03 20:52:13 +0530

    Add joke fetch api

---
+++ script.js
@@ -0,0 +1,14 @@
+const fetchJoke = () => {
+  fetch('https://icanhazdadjoke.com/slack')
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        const jokeElement = document.getElementById('jokeElement');
+
+        jokeElement.innerHTML = jokeText;
+    })
+}
+
+fetchJoke()
+
+setInterval(fetchJoke, 15000)
